fdda432587841ee03600edbd930921c3c6c8e1ce
minord438 <137736999+minord438@users.noreply.github.com>
2024-12-15 13:12:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import mss
 import numpy as np
 from PIL import Image
 import os223ewaegfvd
-aadsfcasdadsaasdfasdfcavdfc
+aadsfcasdadsaasdfasdfcavdfcasdfasd
 def main():
     """asdfasdfsdcaSZ
     Main function for the program
