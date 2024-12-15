376560d56358311f2ce6f440b1840cf9ddce4bc8
minord438 <137736999+minord438@users.noreply.github.com>
2024-12-15 13:13:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     # Max cast is 1.9 secsawfsgvasefdgas
     # Base time it will always cast atasdcx aZSD
     castingBaseTime = 1.0
-    # Max random amount of time to add to the baseasdfgvasdgf
+    # Max random amount of time to add to the baseasdfgvasdgfdaerfvaedrcfv
     castingRandom = .4sdcv aSD
 
     # How long to slack the line
